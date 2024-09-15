d4e81f37fb33187ca4b9053ffa603b1bba6eaeb3
Heero <techheero@gmail.com>
2024-09-14 19:11:20 -0500

    This is the update to config

---
+++ update_config.py
@@ -0,0 +1,19 @@
+import configparser
+import os
+
+# Load the config.ini file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Get the environment variable for the GitHub username
+github_user_name = 'codingwithstrangers'
+gif_frame_duration = '10000'
+
+
+# Update the github_user_name in the config file
+config.set('Settings', 'github_user_name', github_user_name)
+config.set('Settings', 'gif_frame_duration', gif_frame_duration)
+
+# Write the changes back to the config.ini file
+with open('config.ini', 'w') as configfile:
+    config.write(configfile)
